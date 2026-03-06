7f672d04a63427a79265c164119fd344a068add2
Alumno CITEV <alumnog@itson.edu.mx>
2026-03-06 09:51:44 -0700

    Se agrego el welcome.html y se corrigieron los url

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
 #underscore methods and properties
 if __name__ == '__main__':
     app.run()
